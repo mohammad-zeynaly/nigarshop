3554f0e23aa85549bd6d3a43b95336620acbd3af
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-04-22 00:25:36 -0700

    update nigarshop project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container:{
       center: true,
