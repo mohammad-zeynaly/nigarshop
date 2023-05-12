6faf8fe324b5fd145f412fabd36fad0b3d06b133
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-05-12 14:19:49 +0330

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/nigarshop/">{/* basename="/nigarshop" */}
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
